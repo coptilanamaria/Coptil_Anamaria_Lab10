862c99624ff481472244ada249c5f142b450190c
ANAMARIA COPTIL <anamaria.coptil@stud.ubbcluj.ro>
2020-12-11 12:50:51 +0200

    Add project files.

---
+++ Coptil_Anamaria_Lab10/Models/ShopList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Coptil_Anamaria_Lab10.Models
+{
+   public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+    
+
+    }
+
